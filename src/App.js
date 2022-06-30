cbb44ce5b6a83295d3396e39de487993c85b4da8
esadakman <esadd26@gmail.com>
2022-06-30 14:26:33 +0300

    project skeleton completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
-import DatePicker from "react-date-picker";
+import Header from "./components/header/Header";
 
-import { useState } from "react";
+// import { useState } from "react";
 
 function App() {
-  const [value, onChange] = useState(new Date());
   return (
-    <div className="App">
-      <DatePicker onChange={onChange} value={value} />
+    <div className="App  ">
+      <Header />
     </div>
   );
 }
